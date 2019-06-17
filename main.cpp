ce9752aedbee85e601572678ba41212d716513bf
Alexander Tyuryuchkin <tyuryuchkin@gmail.com>
2019-06-17 23:08:20 +0300

    release v0.2.0

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
-    app.setApplicationVersion(QString("0.1.0"));
+    app.setApplicationVersion(QString("0.2.0"));
 
     qmlRegisterType<PodcastListSerialization>("com.podcastplayer.podcastsmodel", 1, 0, "PodcastsModel");
 
